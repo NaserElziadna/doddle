386c33d3bd8b4a60d16006d0b580cb9992066b8f
Naser Elziadna <elziadna10@gmail.com>
2022-02-02 12:19:30 +0200

    fixes and changes
    fixed multi touch to one fingers touch removed appbar , and know it support color from controller

--- lib/doddler.dart
+++ lib/doddler.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'dart:ui' as ui;
 
 import 'package:doddle/draw_controller.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -46,90 +47,88 @@ class _DoddlerState extends State<Doddler> {
       listener: (context, state) {},
       builder: (context, state) {
         if (state is UpdateCanvasState) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Sketcher'),
-              backgroundColor: Colors.purple,
-              actions: [
-                Slider(
-                  value: drawController!.symmetryLines!,
-                  max: 50,
-                  min: 0,
-                  activeColor: Colors.redAccent,
-                  label: "S L",
-                  thumbColor: Colors.black,
-                  inactiveColor: Colors.green,
-                  onChanged: (value) {
-                    setState(() {
-                      drawController =
-                          drawController?.copyWith(symmetryLines: value);
-                      context
-                          .read<DoddlerBloc>()
-                          .add(UpdateSymmetryLines(symmetryLines: value));
-                    });
-                  },
-                )
-              ],
-            ),
-            body: Container(
-              margin: const EdgeInsets.all(20),
+          return Center(
+            child: Container(
+              // width: MediaQuery.of(context).size.width * 0.8,
+              // height: MediaQuery.of(context).size.height * 0.75,
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 150),
               color: Colors.black,
-              child: GestureDetector(
-                onPanUpdate: (DragUpdateDetails details) {
-                  setState(() {
-                    kCanvasSize = Size(
-                        MediaQuery.of(context).size.width,
-                        MediaQuery.of(context).size.height -
-                            (AppBar().preferredSize.height));
-                    const pinSpace = 100;
+              child: RawGestureDetector(
+                behavior: HitTestBehavior.opaque,
+                gestures: <Type, GestureRecognizerFactory>{
+                  _SingleGestureRecognizer:
+                      GestureRecognizerFactoryWithHandlers<
+                          _SingleGestureRecognizer>(
+                    () => _SingleGestureRecognizer(debugOwner: this),
+                    (instance) {
+                      // instance.onStart = (position) => _startPath(position);
+                      instance.onUpdate = (position) {
+                        setState(() {
+                          kCanvasSize = Size(
+                              MediaQuery.of(context).size.width,
+                              MediaQuery.of(context).size.height -
+                                  (AppBar().preferredSize.height));
+                          const pinSpace = 50;
 
-                    RenderBox box = context.findRenderObject() as RenderBox;
-                    Offset point = box.globalToLocal(details.globalPosition);
+                          // RenderBox box =
+                          // context.findRenderObject() as RenderBox;
+                          // Offset point =
+                          // box.globalToLocal(details.globalPosition);
+                          Offset point = position;
 
-                    point = point.translate(-(kCanvasSize.width / 2),
-                        -((kCanvasSize.height / 2) + pinSpace));
+                          point = point.translate(-(kCanvasSize.width / 2),
+                              -((kCanvasSize.height / 2) + pinSpace));
 
-                    print("x = ${point.dx} , y = ${point.dy}");
-                    BlocProvider.of<DoddlerBloc>(context)
-                        .add(AddPointEvent(Point(
-                            offset: point,
-                            paint: Paint()
-                              ..color = state.drawController == null
-                                  ? Colors.white
-                                  : state.drawController!.currentColor
-                              ..strokeCap = StrokeCap.round
-                              ..strokeJoin = StrokeJoin.miter
-                              ..strokeWidth = (Random().nextInt(10)) * 1.0)));
-                  });
-                },
-                onPanEnd: (DragEndDetails details) {
-                  BlocProvider.of<DoddlerBloc>(context)
-                        .add(AddPointEvent(Point(
-                            offset: null,
-                            paint: Paint()
-                              ..color = Color.fromRGBO(
-                                  Random().nextInt(255),
-                                  Random().nextInt(255),
-                                  Random().nextInt(255),
-                                  1)
-                              ..strokeCap = StrokeCap.square
-                              ..strokeJoin = StrokeJoin.bevel
-                              ..strokeWidth = (Random().nextInt(10)) * 1.0),end: true));
-                      //         context
-                      // .read<DoddlerBloc>()
-                      // .add(TakePageStampEvent(globalKey!));
+                          print("x = ${point.dx} , y = ${point.dy}");
+                          BlocProvider.of<DoddlerBloc>(context).add(
+                              AddPointEvent(Point(
+                                  offset: point,
+                                  paint: Paint()
+                                    ..color = state.drawController == null
+                                        ? Colors.white
+                                        : state.drawController!.currentColor
+                                    ..strokeCap = StrokeCap.round
+                                    ..strokeJoin = StrokeJoin.miter
+                                    ..strokeWidth =
+                                        (Random().nextInt(10)) * 1.0)));
+                        });
+                      };
+                      instance.onEnd = (position) => {
+                            BlocProvider.of<DoddlerBloc>(context).add(
+                                AddPointEvent(
+                                    Point(
+                                        offset: null,
+                                        paint: Paint()
+                                          ..color = Color.fromRGBO(
+                                              Random().nextInt(255),
+                                              Random().nextInt(255),
+                                              Random().nextInt(255),
+                                              1)
+                                          ..strokeCap = StrokeCap.square
+                                          ..strokeJoin = StrokeJoin.bevel
+                                          ..strokeWidth =
+                                              (Random().nextInt(10)) * 1.0),
+                                    end: true))
+                            //         context
+                            // .read<DoddlerBloc>()
+                            // .add(TakePageStampEvent(globalKey!));
+                          };
+                    },
+                  ),
                 },
                 child: RepaintBoundary(
                   key: globalKey,
                   child: ClipRect(
                     child: CustomPaint(
                       foregroundPainter: Sketcher(
-                        state.drawController?.points ?? [],
-                        kCanvasSize,
-                        state.drawController != null
-                            ? state.drawController!.symmetryLines!
-                            : 5,
-                      ),
+                          state.drawController?.points ?? [],
+                          kCanvasSize,
+                          state.drawController != null
+                              ? state.drawController!.symmetryLines!
+                              : 5,
+                          state.drawController != null
+                              ? state.drawController!.currentColor
+                              : Colors.red),
                       painter: LastImageAsBackground(
                         image: state.drawController == null
                             ? null
@@ -149,7 +148,6 @@ class _DoddlerState extends State<Doddler> {
                 ),
               ),
             ),
-            bottomSheet: const ToolsWidget(),
           );
         }
         return const Center(
@@ -162,6 +160,54 @@ class _DoddlerState extends State<Doddler> {
   }
 }
 
+/// [GestureRecognizer] that allows just one input pointer.
+class _SingleGestureRecognizer extends OneSequenceGestureRecognizer {
+  @override
+  String get debugDescription => 'single_gesture_recognizer';
+
+  ValueChanged<Offset>? onStart;
+  ValueChanged<Offset>? onUpdate;
+  ValueChanged<Offset>? onEnd;
+
+  bool pointerActive = false;
+
+  _SingleGestureRecognizer({
+    Object? debugOwner,
+    PointerDeviceKind? kind,
+  }) : super(
+          debugOwner: debugOwner,
+          kind: kind,
+        );
+
+  @override
+  void addAllowedPointer(PointerDownEvent event) {
+    if (pointerActive) {
+      return;
+    }
+
+    startTrackingPointer(event.pointer, event.transform);
+  }
+
+  @override
+  void handleEvent(PointerEvent event) {
+    if (event is PointerMoveEvent) {
+      onUpdate?.call(event.position);
+    } else if (event is PointerDownEvent) {
+      pointerActive = true;
+      onStart?.call(event.position);
+    } else if (event is PointerUpEvent) {
+      pointerActive = false;
+      onEnd?.call(event.position);
+    } else if (event is PointerCancelEvent) {
+      pointerActive = false;
+      onEnd?.call(event.position);
+    }
+  }
+
+  @override
+  void didStopTrackingLastPointer(int pointer) {}
+}
+
 class LastImageAsBackground extends CustomPainter {
   ui.Image? image;
   LastImageAsBackground({
@@ -171,10 +217,7 @@ class LastImageAsBackground extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     if (image != null) {
       print("=entered=");
-      canvas.drawImage(
-          image!,
-          Offset.zero,
-          Paint());
+      canvas.drawImage(image!, Offset.zero, Paint());
     }
     print("=out=");
   }
@@ -189,26 +232,18 @@ class Sketcher extends CustomPainter {
   final List<Point?> points;
   final Size screenSize;
   final double symmetryLines;
+  final Color color;
 
-  Sketcher(this.points, this.screenSize, this.symmetryLines);
+  Sketcher(this.points, this.screenSize, this.symmetryLines, this.color);
 
   @override
   bool shouldRepaint(Sketcher oldDelegate) {
     return true;
   }
 
-  // List<Offset> doS() {
-  //   final b = [];
-  //   for (final c in points) {
-  //     b.add(c?.offset);
-  //   }
-  //   return [];
-  // }
-
   @override
   void paint(Canvas canvas, Size size) {
     print(size.toString());
-    // canvas.drawPicture(picture)
     canvas.translate(size.width / 2, size.height / 2);
 
     Paint paint = Paint()
@@ -217,7 +252,6 @@ class Sketcher extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeWidth = 4.0;
 
-    // const symmetryLines = 0;
     var angle = 360 / (symmetryLines);
 
     print(angle);
@@ -225,21 +259,6 @@ class Sketcher extends CustomPainter {
     for (var j = 0; j < points.length - 1; j++) {
       if (points[j + 1] != null) {
         if (points[j]!.offset != null && points[j + 1]!.offset != null) {
-          //Draw Rectangles
-          // Rect myRect = Rect.fromLTWH(
-          //     points[j]!.offset!.dx, points[j]!.offset!.dy, 3.0, 4.0);
-
-          // canvas.drawRect(myRect, points[j]!.paint ?? paint);
-
-          // Glow Effect
-          // canvas.drawLine(
-          //     points[j]!.offset!,
-          //     points[j + 1]!.offset!,
-          //     Paint()
-          //       ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 3.0)
-          //       ..color = Colors.red.withAlpha(255)
-          //       ..strokeWidth = 10.0);
-
           path.moveTo(points[j]!.offset!.dx, points[j]!.offset!.dy);
           path.lineTo(points[j + 1]!.offset!.dx, points[j + 1]!.offset!.dy);
         }
@@ -248,23 +267,18 @@ class Sketcher extends CustomPainter {
       }
     }
 
+    print(color);
     for (var i = 0; i <= symmetryLines; i++) {
       canvas.drawPath(
           path,
           Paint()
             ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 10.0)
-            ..color = Color.fromRGBO(
-                                  Random().nextInt(255),
-                                  Random().nextInt(255),
-                                  Random().nextInt(255),
-                                  1)
+            ..color = color
             ..style = PaintingStyle.stroke
             ..strokeWidth = 10.0);
 
       canvas.drawPath(path, paint);
       canvas.rotate(angle);
     }
-
-    // print("canvas.getSaveCount() = ${canvas.getSaveCount()}");
   }
 }
