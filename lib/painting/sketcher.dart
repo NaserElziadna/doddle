210af09662fdd6a27b7c1b6c0e9d5e284bc85161
Naser Elziadna <elziadna10@gmail.com>
2022-02-08 15:18:00 +0200

    draw according to current pen tool

--- lib/painting/sketcher.dart
+++ lib/painting/sketcher.dart
@@ -2,6 +2,7 @@ import 'package:doddle/generated/assets.gen.dart';
 import 'package:doddle/models/draw_controller.dart';
 import 'package:doddle/models/point.dart';
 import 'package:flutter/material.dart';
+import 'package:path_drawing/path_drawing.dart';
 
 import 'shapes.dart';
 
@@ -22,89 +23,104 @@ class Sketcher extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    print(size.toString());
+    print("penTool =  $penTool");
     canvas.translate(size.width / 2, size.height / 2);
 
     Paint paint = Paint()
       ..color = Colors.white
       ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 4.0;
+      ..strokeWidth = 2.0;
 
     var angle = 360 / (symmetryLines);
 
-    print(angle);
     Path path = Path();
     for (var j = 0; j < points.length - 1; j++) {
       if (points[j + 1] != null) {
         if (points[j]!.offset != null && points[j + 1]!.offset != null) {
-          if (penTool == PenTool.normalPen || penTool == PenTool.glowPen) {
-            path.moveTo(points[j]!.offset!.dx, points[j]!.offset!.dy);
-            path.lineTo(points[j + 1]!.offset!.dx, points[j + 1]!.offset!.dy);
-
-            // Shapes aa = Shapes(
-            //     canvas: canvas, center: points[j]!.offset!, paint: paint);
-            // aa.drawType(ShapeType.Star5);
-
-          }
-
-          // Shapes shapes = Shapes(
-          //     canvas: canvas,
-          //     radius: 10,
-          //     paint: Paint()
-          //       ..color = color
-          //       ..style = PaintingStyle.stroke
-          //       ..strokeWidth = 3.0,
-          //     center: points[j]!.offset!,
-          //     angle: 0);
-
-          // shapes.drawType(ShapeType.Triangle); // enum
+          path.moveTo(points[j]!.offset!.dx, points[j]!.offset!.dy);
+          path.lineTo(points[j + 1]!.offset!.dx, points[j + 1]!.offset!.dy);
         }
       } else {
         j++;
       }
     }
+    if (penTool == PenTool.glowPen ||
+        penTool == PenTool.normalPen ||
+        penTool == PenTool.normalWithShaderPen ||
+        penTool == PenTool.glowWithDotsPen) {
+      for (var i = 0; i <= symmetryLines; i++) {
+        if (penTool == PenTool.glowPen) {
+          canvas.drawPath(
+              path,
+              Paint()
+                ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 5.0)
+                ..color = color
+                ..style = PaintingStyle.stroke
+                ..strokeWidth = 5.0);
 
-    print(color);
+          canvas.drawPath(path, paint);
+        } else if (penTool == PenTool.normalPen) {
+          canvas.drawPath(
+              path,
+              Paint()
+                ..color = color
+                ..strokeJoin = StrokeJoin.round
+                ..style = PaintingStyle.stroke
+                ..strokeWidth = 4.0);
+        } else if (penTool == PenTool.normalWithShaderPen) {
+          canvas.drawPath(
+              path,
+              Paint()
+                ..color = color
+                ..strokeJoin = StrokeJoin.round
+                ..style = PaintingStyle.stroke
+                ..shader = sweepShader
+                ..strokeWidth = 4.0);
+        } else if (penTool == PenTool.glowWithDotsPen) {
+          canvas.drawPath(
+              path,
+              Paint()
+                ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 5.0)
+                ..color = color
+                ..style = PaintingStyle.stroke
+                ..strokeWidth = 5.0);
 
-    for (var i = 0; i <= symmetryLines; i++) {
-      if (penTool == PenTool.glowPen) {
-        canvas.drawPath(
-            path,
-            Paint()
-              ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 10.0)
-              ..color = color
-              ..style = PaintingStyle.stroke
-              // ..shader = sweepShader
-              ..strokeWidth = 10.0);
-
-        canvas.drawPath(path, paint);
-      } else if (penTool == PenTool.normalPen) {
-        canvas.drawPath(
-            path,
-            Paint()
-              ..color = color
-              ..strokeJoin = StrokeJoin.round
-              ..style = PaintingStyle.stroke
-              // ..shader = sweepShader
-              ..strokeWidth = 5.0);
+          canvas.drawPath(
+              dashPath(
+                path,
+                dashArray:
+                    CircularIntervalList<double>(<double>[100.0, 0.0, 50.0]),
+              ),
+              paint);
+        }
+        canvas.rotate(angle);
       }
-      canvas.rotate(angle);
+    } else {
+      //eraser
+      canvas.drawPath(
+        path,
+        Paint()
+          ..color = Colors.black
+          ..strokeJoin = StrokeJoin.round
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 20.0,
+      );
     }
   }
 }
 
-//  const SweepGradient colorWheelGradient =
-//             SweepGradient(center: Alignment.bottomRight, colors: [
-//           Color.fromARGB(255, 255, 0, 0),
-//           Color.fromARGB(255, 255, 255, 0),
-//           Color.fromARGB(255, 0, 255, 0),
-//           Color.fromARGB(255, 0, 255, 255),
-//           Color.fromARGB(255, 0, 0, 255),
-//           Color.fromARGB(255, 255, 0, 255),
-//           Color.fromARGB(255, 255, 0, 0),
-//         ]);
-//         // If we create a shader from the above SweepGraident, we get
-//         // a crash on web, but only on web.
-//         final Shader sweepShader =
-//             colorWheelGradient.createShader(const Rect.fromLTWH(0, 0, 20, 10));
+const SweepGradient colorWheelGradient =
+    SweepGradient(center: Alignment.bottomRight, colors: [
+  Color.fromARGB(255, 255, 0, 0),
+  Color.fromARGB(255, 255, 255, 0),
+  Color.fromARGB(255, 0, 255, 0),
+  Color.fromARGB(255, 0, 255, 255),
+  Color.fromARGB(255, 0, 0, 255),
+  Color.fromARGB(255, 255, 0, 255),
+  Color.fromARGB(255, 255, 0, 0),
+]);
+// If we create a shader from the above SweepGraident, we get
+// a crash on web, but only on web.
+final Shader sweepShader =
+    colorWheelGradient.createShader(const Rect.fromLTWH(0, 0, 100, 10));
