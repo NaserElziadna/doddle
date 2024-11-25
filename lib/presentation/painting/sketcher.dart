23d28a1cc69a858de0a9f72263e7c218cbec74c2
Naser Elziadna <elziadna10@gmail.com>
2024-11-25 05:16:00 +0200

    fix the symmtry line drawing

--- lib/presentation/painting/sketcher.dart
+++ lib/presentation/painting/sketcher.dart
@@ -2,9 +2,11 @@ import 'package:doddle/domain/models/draw_controller.dart';
 import 'package:doddle/domain/models/point.dart';
 import 'package:flutter/material.dart';
 import 'package:path_drawing/path_drawing.dart';
-
+import 'dart:math' as math;
 
 import 'shapes.dart';
+
+const pi = math.pi;
 
 class Sketcher extends CustomPainter {
   final List<Point?> points;
@@ -12,8 +14,9 @@ class Sketcher extends CustomPainter {
   final double symmetryLines;
   final Color color;
   final PenTool penTool;
-  List<Offset> randomOffsets = [];
   final double penSize;
+  final bool mirrorSymmetry;
+  final bool showGuidelines;
 
   Sketcher(
     this.points,
@@ -21,8 +24,10 @@ class Sketcher extends CustomPainter {
     this.symmetryLines,
     this.color,
     this.penTool,
-    this.penSize,
-  );
+    this.penSize, {
+    this.mirrorSymmetry = false,
+    this.showGuidelines = true,
+  });
 
   @override
   bool shouldRepaint(Sketcher oldDelegate) {
@@ -31,17 +36,69 @@ class Sketcher extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.translate(size.width / 2, size.height / 2);
+    canvas.save();
+    final center = Offset(size.width / 2, size.height / 2);
+    canvas.translate(center.dx, center.dy);
+
+    if (showGuidelines) {
+      _drawGuidelines(canvas, size);
+    }
 
     Paint paint = Paint()
-      ..color = Colors.white
+      ..color = color
       ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke
       ..strokeWidth = penSize;
 
     Path path = Path();
     _drawPoints(canvas, path, paint);
-    _applyPenEffects(canvas, path, paint);
+    
+    for (var i = 0; i < symmetryLines; i++) {
+      canvas.save();
+      _drawPathWithEffect(canvas, path, paint);
+      canvas.restore();
+      canvas.rotate(2 * pi / symmetryLines);
+    }
+
+    canvas.restore();
+  }
+
+  void _drawGuidelines(Canvas canvas, Size size) {
+    final guidelinePaint = Paint()
+      ..color = Colors.blue.withOpacity(0.3)
+      ..strokeWidth = 1
+      ..style = PaintingStyle.stroke;
+
+    final radius = math.min(size.width, size.height) / 2 - penSize;
+
+    canvas.drawCircle(Offset.zero, radius, guidelinePaint);
+
+    for (int i = 0; i < symmetryLines; i++) {
+      final angle = i * 2 * pi / symmetryLines;
+      final x = math.cos(angle) * radius;
+      final y = math.sin(angle) * radius;
+      canvas.drawLine(Offset.zero, Offset(x, y), guidelinePaint);
+    }
+  }
+
+  List<Offset> _getSymmetryPoints(Offset point) {
+    final relX = point.dx;
+    final relY = point.dy;
+    final dist = math.sqrt(relX * relX + relY * relY);
+    final angle = math.atan2(relX, relY);
+    
+    List<Offset> result = [];
+    for (int i = 0; i < symmetryLines; i++) {
+      final theta = angle + 2 * pi * i / symmetryLines;
+      final x = math.sin(theta) * dist;
+      final y = math.cos(theta) * dist;
+      result.add(Offset(x, y));
+      
+      if (mirrorSymmetry) {
+        result.add(Offset(-x, y));
+      }
+    }
+    return result;
   }
 
   void _drawPoints(Canvas canvas, Path path, Paint paint) {
@@ -57,7 +114,10 @@ class Sketcher extends CustomPainter {
       if (currentPoint != null && nextPoint != null) {
         if (penTool == PenTool.customPen) {
           for (var offset in points[j]!.randomOffset!) {
-            canvas.drawRect(offset & const Size(1.0, 1.0), paint);
+            final symmetryOffsets = _getSymmetryPoints(offset);
+            for (var symOffset in symmetryOffsets) {
+              canvas.drawRect(symOffset & const Size(1.0, 1.0), paint);
+            }
           }
         } else {
           path.moveTo(currentPoint.dx, currentPoint.dy);
@@ -80,10 +140,7 @@ class Sketcher extends CustomPainter {
       return;
     }
 
-    for (var i = 0; i < symmetryLines; i++) {
-      _drawPathWithEffect(canvas, path, paint);
-      performSymmetryLines(canvas, screenSize, symmetryLines);
-    }
+    _drawPathWithEffect(canvas, path, paint);
   }
 
   void _drawPathWithEffect(Canvas canvas, Path path, Paint paint) {
@@ -106,7 +163,6 @@ class Sketcher extends CustomPainter {
   }
 
   void _drawGlowPath(Canvas canvas, Path path, Paint paint) {
-    // Outer glow
     canvas.drawPath(
       path,
       paint
@@ -115,7 +171,6 @@ class Sketcher extends CustomPainter {
         ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 10),
     );
 
-    // Middle glow
     canvas.drawPath(
       path,
       paint
@@ -124,7 +179,6 @@ class Sketcher extends CustomPainter {
         ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 5),
     );
 
-    // Core line
     canvas.drawPath(
       path,
       paint
@@ -157,10 +211,8 @@ class Sketcher extends CustomPainter {
   }
 
   void _drawGlowDotsPath(Canvas canvas, Path path, Paint paint) {
-    // Draw the base glow path
     _drawGlowPath(canvas, path, paint);
 
-    // Add dots along the path
     for (var i = 0.0; i <= 1.0; i += 0.1) {
       final metric = path.computeMetrics().first;
       final tangent = metric.getTangentForOffset(metric.length * i);
@@ -178,41 +230,6 @@ class Sketcher extends CustomPainter {
     }
   }
 }
-
-performSymmetryLines(Canvas canvas, Size size, double symmetryLines) {
-  if (symmetryLines == 2) {
-    canvas.translate(size.width / 2, 0);
-  } else if (symmetryLines == 3) {
-    canvas.rotate(180 / 12.2);
-  } else if (symmetryLines == 4) {
-    canvas.rotate(360 / 4.5);
-  } else if (symmetryLines == 8) {
-    canvas.rotate(360 / 4.5);
-  } else if (symmetryLines == 5) {
-    canvas.rotate(180 / 5.5);
-  } else if (symmetryLines == 10) {
-    canvas.rotate(180 / 5.5);
-  } else if (symmetryLines == 6) {
-    canvas.rotate(360 / 8);
-  }
-  // else if (symmetryLines == 12) {
-  //   canvas.rotate(360 / 8);
-  // }else if (symmetryLines == 8) {
-  //   canvas.rotate(360/180);
-  // }
-  else {
-    canvas.rotate(360 / symmetryLines);
-  }
-}
-
-// double getAngle(double symmetryLines) {
-//   if (symmetryLines == 1) {
-//     return 360 / (symmetryLines);
-//   } else if (symmetryLines == 2) {
-//     return 360;
-//   }
-//   return 360 / (symmetryLines);
-// }
 
 const SweepGradient colorWheelGradient =
     SweepGradient(center: Alignment.bottomRight, colors: [
